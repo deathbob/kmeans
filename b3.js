ec9cf14e9abf3c7d14d25a685bb4a7b1fdca074d
Bob Larrick <larrick@gmail.com>
2015-01-27 23:56:59 -0500

    moving the balls

--- b3.js
+++ b3.js
@@ -1,3 +1,4 @@
+
 //scene and camera
 
 var scene = new THREE.Scene();
@@ -53,6 +54,31 @@ var geometry = new THREE.BoxGeometry( squareSize, squareSize, squareSize );
 
 var cubes = [];
 
+var centroidMulti = 255 * (squareSize / 2);
+
+var centroids = [
+  {
+    x: Math.random() * centroidMulti,
+    y: Math.random() * centroidMulti,
+    z: Math.random() * centroidMulti,
+    pixels: []
+  },
+  {
+    x: Math.random() * centroidMulti,
+    y: Math.random() * centroidMulti,
+    z: Math.random() * centroidMulti,
+    pixels: []
+  },
+  {
+    x: Math.random() * centroidMulti,
+    y: Math.random() * centroidMulti,
+    z: Math.random() * centroidMulti,
+    pixels: []
+  }
+]
+
+
+
 function allCubes(pixels){
   cubes = [];
   scene.clear();
@@ -70,6 +96,17 @@ function allCubes(pixels){
     cubes.push(cube);
 
   }
+
+  centroids.forEach(function(centroid){
+    var geometry = new THREE.SphereGeometry(50);
+    var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    var sphere = new THREE.Mesh( geometry, material );
+    sphere.position.x = centroid.x
+    sphere.position.y = centroid.y
+    sphere.position.z = centroid.z
+    centroid.sphere = sphere
+    scene.add( sphere );
+  })
 }
 
 function explode(){
@@ -99,6 +136,69 @@ function explode(){
         // })
         .start();
   }
+}
+
+function square(num){
+  return Math.pow(num, 2);
+}
+
+function dist_cube_to_centroid(cube, centroid){
+  var x, y, z;
+  x = cube.position.x - centroid.sphere.position.x
+  y = cube.position.y - centroid.sphere.position.y
+  z = cube.position.z - centroid.sphere.position.z
+  var res = square(x) + square(y) + square(z);
+  return Math.sqrt(res);
+}
+
+function kMeansIter(){
+  // pixels is a global
+  // cubes is a global
+  var mindex;
+  var index;
+  var distances = cubes.map(function(cube){
+    return centroids.map(function(centroid){
+      return dist_cube_to_centroid(cube, centroid);
+    })
+  })
+  //  clear centroid pixels array between iters
+  centroids.forEach(function(cen){ cen.pixels = [] });
+  var mindexs = distances.map(function(distances_array, pixel_index){
+    var l_min = _.min(distances_array);
+    var closest_centroid = distances_array.indexOf(l_min);
+    centroids[closest_centroid].pixels.push(pixel_index)
+  })
+
+  // reduce pixels distances for each centroid, recompute
+  return true;
+}
+
+function centroidsRecompute(){
+  centroids.forEach(function(centroid){
+    var pixel_length = centroid.pixels.length
+    var x, y, z;
+    if(pixel_length > 0){
+      x = centroid.pixels.reduce(function(prev, curr){ return prev + cubes[curr].position.x }, 0) / pixel_length
+      y = centroid.pixels.reduce(function(prev, curr){ return prev + cubes[curr].position.y }, 0) / pixel_length
+      z = centroid.pixels.reduce(function(prev, curr){ return prev + cubes[curr].position.z }, 0) / pixel_length
+    }
+    var target = {x: x, y: y, z: z};
+    var col = "rgb(" + x + "," + y + "," + z + ")"
+    var tween = new TWEEN.Tween(centroid.sphere.position)
+        .to(target, 2000)
+        .onUpdate( function() {
+          centroid.sphere.position.x = this.x;
+          centroid.sphere.position.y = this.y;
+          centroid.sphere.position.z = this.z;
+        })
+        .start();
+
+//    centroid.x = x;
+//    centroid.y = y;
+//    centroid.z = z;
+  });
+  return true
+
 }
 
 function implode(){
